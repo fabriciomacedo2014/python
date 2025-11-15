0334578df46545ccde4ef800a9a21a9a5967b27a
fabriciomacedo2014 <fabriciodacostamacedo25@gmail.com>
2025-11-14 22:36:39 -0400

    Delete python_pratice directory

--- python_pratice/par_ou_impar.py
+++
@@ -1,9 +0,0 @@
-num = int(input('Digite um número; (0 para parar)'))
-while num != 0:
-    if num % 2 == 0:
-        print('Par')
-        num = int(input('Digite um número; (0 para parar)'))
-    elif num % 2 != 0:
-        print('impar')
-        num = int(input('Digite um número; (0 para parar)'))
-print('Finalizado!')
